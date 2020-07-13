600926e00f98da0f36346fcba58f5480f94e3c53
msaaddev <mrsaadirfan@gmail.com>
2020-07-14 04:09:07 +0500

    📦 NEW: tagline

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,9 +5,11 @@ import '../styles/homepage.css';
 
 const Homepage = () => {
     return (
-        <div className='TT_container'>
-            <div className='sub_container'>
-                <img src={home} alt={data.homepage_img_alt} id='TT_homepage_img' />
+        <div className='hp_container'>
+            <div className='hp_sub_container_1'>
+                <img src={home} alt={data.homepage_img_alt} id='hp_homepage_img' />
+                <h1 id='tagline'>{data.tag_line}</h1>
+            </div>
             </div>
             <div className='TT_sub_container'></div>
         </div>
