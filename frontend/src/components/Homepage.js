b74ba4addec94e3e363f008b13ba2ac74e34e5e2
msaaddev <mrsaadirfan@gmail.com>
2020-07-14 04:09:33 +0500

    📦 NEW: sign up form

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -10,8 +10,25 @@ const Homepage = () => {
                 <img src={home} alt={data.homepage_img_alt} id='hp_homepage_img' />
                 <h1 id='tagline'>{data.tag_line}</h1>
             </div>
+            <div className='hp_sub_container_2'>
+                <div className='hp_sign_up'>
+                    <h2>{data.create_account}</h2>
+                    <div className='hp_sign_up_inputs'>
+                        <div className='hp_align_name'>
+                            <label htmlFor='Name'>Name</label>
+                            <input type='text' />
+                        </div>
+                        <div className='hp_align_email'>
+                            <label htmlFor='Email'>Email</label>
+                            <input type='text' />
+                        </div>
+                        <div className='hp_align_password'>
+                            <label htmlFor='Password'>Password</label>
+                            <input type='password' />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='TT_sub_container'></div>
         </div>
     );
 };
