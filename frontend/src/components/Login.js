357edf18d7d52872a78386760095d80451ddc0ad
RazeMuse <56264662+RazeMuse@users.noreply.github.com>
2020-07-14 23:39:14 +0500

    📦 NEW: imported css image & component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import data from '../data/data.json';
+import '../styles/login.css';
+import login from '../images/login.png';
+import Nav from './Nav';
 
 const Login = () => {
     return (  );
