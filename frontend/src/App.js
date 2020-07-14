72c9d35a905f4c6b35710057b4b351a19a3f0385
RazeMuse <56264662+RazeMuse@users.noreply.github.com>
2020-07-14 23:37:30 +0500

    📦 NEW: login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Homepage from './components/Homepage';
+import Login from './components/Login';
 
 function App() {
     return (
         <>
-            <Homepage />
+            <Login />
         </>
     );
 }
