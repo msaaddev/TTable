edb036e22aa952b339a436ba5a49cc8a354ea1fd
RazeMuse <56264662+RazeMuse@users.noreply.github.com>
2020-07-14 23:40:33 +0500

    📦 NEW: login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,7 +5,30 @@ import login from '../images/login.png';
 import Nav from './Nav';
 
 const Login = () => {
-    return (  );
-}
- 
+    return (
+        <div className='lgn_container'>
+            <div className='lgn_subcontainer_2'>
+                <div className='lgn_login_form'>
+                    <div className='lgn_input_fields'>
+                        <div className='lgn_align_email'>
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' id='lgn_signin_email' />
+                        </div>
+                        <div className='lgn_align_password'>
+                            <label htmlFor='password'>Password</label>
+                            <input type='password' id='lgn_signin_password' />
+                        </div>
+                    </div>
+                    <div className='lgn_btn'>
+                        <button>Login</button>
+                    </div>
+                </div>
+                <div className='lgn_img'>
+                    <img src={login} alt='login_image' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export default Login;
