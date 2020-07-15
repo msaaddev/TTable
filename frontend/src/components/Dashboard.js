93d30d35d5a499df703d97a459af15acce54b1d3
msaaddev <mrsaadirfan@gmail.com>
2020-07-15 20:45:10 +0500

    📦 NEW: dashboard layout

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,6 +8,25 @@ const Dashboard = () => {
     return (
         <div className='db_container'>
             <Nav userName='John Doe' appName={data.app_name} />
+            <div className='db_subcontainer'>
+                <div className='db_subcontainer_tables'>
+                    <div className='db_timetable'>
+                        <p>{data.section_a}</p>
+                        <button id='db_section_a'>View</button>
+                    </div>
+                    <div className='db_timetable'>
+                        <p>{data.section_b}</p>
+                        <button id='db_section_b'>View</button>
+                    </div>
+                    <div className='db_timetable'>
+                        <p>{data.section_c}</p>
+                        <button id='db_section_c'>View</button>
+                    </div>
+                    <div className='db_timetable_btn'>
+                        <button id='db_create_table'>Create Timetable!</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
