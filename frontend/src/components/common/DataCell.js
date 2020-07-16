e8f89220ef076b16d0e9f4e5ae05a3330e60fa00
RazeMuse <56264662+RazeMuse@users.noreply.github.com>
2020-07-16 22:57:39 +0500

    👌 IMPROVE: conditional rendering

--- frontend/src/components/common/DataCell.js
+++ frontend/src/components/common/DataCell.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const DataCell = ({ room, section, session }) => {
+const DataCell = ({ info_1, info_2, info_3, info_4, info_5, info_6 }) => {
     return (
         <tr>
-            <td>{room}</td>
-            <td>{section}</td>
-            <td>{session}</td>
+            <td>{info_1}</td>
+            <td>{info_2}</td>
+            <td>{info_3}</td>
+            {info_4 !== '' && info_5 !== '' && info_6 !== '' && (
+                <>
+                    <td>{info_4}</td>
+                    <td>{info_5}</td>
+                    <td>{info_6}</td>
+                </>
+            )}
         </tr>
     );
 };
--- frontend/src/components/common/DisplayTableData.js
+++ frontend/src/components/common/DisplayTableData.js
@@ -2,7 +2,16 @@ import React from 'react';
 import DataCell from '../common/DataCell';
 import '../../styles/displaytabledata.css';
 
-const DisplayTableData = ({ roomInfo, heading_1, heading_2, heading_3 }) => {
+const DisplayTableData = ({
+    info,
+    heading_1,
+    heading_2,
+    heading_3,
+    heading_4,
+    heading_5,
+    heading_6,
+}) => {
+    console.log(info[0].prof_name);
     return (
         <table className='dtd_container'>
             <thead>
@@ -10,17 +19,40 @@ const DisplayTableData = ({ roomInfo, heading_1, heading_2, heading_3 }) => {
                     <th>{heading_1}</th>
                     <th>{heading_2}</th>
                     <th>{heading_3}</th>
+                    {heading_4 !== '' && heading_5 !== '' && heading_6 !== '' && (
+                        <>
+                            <th>{heading_4}</th>
+                            <th>{heading_5}</th>
+                            <th>{heading_6}</th>
+                        </>
+                    )}
                 </tr>
             </thead>
             <tbody>
-                {roomInfo.map((map) => (
-                    <DataCell
-                        key={map.room}
-                        room={map.room}
-                        section={map.section}
-                        session={map.session}
-                    />
-                ))}
+                {info[0].prof_name === undefined &&
+                    info.map((map) => (
+                        <DataCell
+                            key={map.room}
+                            info_1={map.room}
+                            info_2={map.section}
+                            info_3={map.session}
+                            info_4=''
+                            info_5=''
+                            info_6=''
+                        />
+                    ))}
+                {info[0].prof_name !== undefined &&
+                    info.map((map) => (
+                        <DataCell
+                            key={map.course_ID}
+                            info_1={map.prof_name}
+                            info_2={map.course_name}
+                            info_3={map.course_ID}
+                            info_4={map.section}
+                            info_5={map.session}
+                            info_6={map.credit_hrs}
+                        />
+                    ))}
             </tbody>
         </table>
     );
