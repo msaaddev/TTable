d7788c2306e13a667b2d76c5de39c1c54a52e185
msaaddev <mrsaadirfan@gmail.com>
2020-07-23 16:48:26 +0500

    👌 IMPROVE: linked database

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const roomModel = require('../models/room');
 const router = express.Router();
 
-// route for getting rooms
+// route for sending rooms information in the database
 router.post('/', (req, res) => {
-    console.log(req.body);
+    roomModel.createRoomData(req.body);
+});
+
+// route for getting room information in the database
+router.get('/', async (req, res) => {
+    console.log(req.query.email);
+    const data = await roomModel.getRoomData(req.query.email);
+    if (data !== false) res.send(data);
+    else res.status(404).send('Nothing Found!');
 });
 
 module.exports = router;
