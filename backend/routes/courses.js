b6b142b892464323ca7e734e70c9bb864bdd8d01
msaaddev <mrsaadirfan@gmail.com>
2020-07-23 17:17:40 +0500

    📦 NEW: fetching database data route

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const courseModel = require('../models/courses');
 const router = express.Router();
 
-// route for getting rooms
+// route for sending courses into database
 router.post('/', (req, res) => {
+    courseModel.createCourseData(req.body);
     console.log(req.body);
+});
+
+// route for getting course information in the database
+router.get('/', async (req, res) => {
+    const data = await courseModel.getCourseData(req.query.email);
+    if (data !== false) res.send(data);
+    else res.statusCode(404).send('Nothing Found!');
 });
 
 module.exports = router;
