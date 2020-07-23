8987b59bbf06510e592755c34365ae0c20e68962
msaaddev <mrsaadirfan@gmail.com>
2020-07-24 01:13:56 +0500

    👌 IMPROVE: routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const user = require('../models/user');
 const router = express.Router();
 
 // route for getting user
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.get('/', async (req, res) => {
+    const userData = await user.getUserData(req.query.token);
+
+    if (userData === false) res.send('false');
+    else {
+        const obj = {
+            email: userData[0].userAccount,
+            username: userData[0].username,
+        };
+        res.send(obj);
+    }
 });
 
 module.exports = router;
