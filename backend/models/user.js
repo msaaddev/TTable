cd39887ebcf9b0defbc7a57dad0b1eec99ef7327
msaaddev <mrsaadirfan@gmail.com>
2020-07-24 03:08:19 +0500

    👌 IMPROVE: code

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,32 +21,26 @@ const User = mongoose.model('UserInfo', userInfo);
  *
  * stores user information in the database
  */
-const createUser = async () => {
-    const userData = new User({
-        userAccount: 'moosaraza@gmail.com',
-        password: '0MRBAGlMbLKNqZl$X',
-        username: 'Moosa Raza',
-    });
+const createUser = async (obj) => {
+    const userData = new User(obj);
 
     // finding if there is already a document exists with this credentials
-    const response = await User.find({ userAccount: 'moosaraza@gmail.com' }).countDocuments();
+    const response = await User.find({ userAccount: obj.userAccount }).countDocuments();
 
     // If there is already a user document exists then send an error
     if (response > 0) {
-        console.log('Cannot create more than one user for same email address.');
-        return;
+        return false;
     }
     const result = await userData.save();
+    return true;
 };
 
 // getting user information
 const getUserData = async (token) => {
     const user = decodingJWT(token);
-
-    const userData = await User.find({ userAccount: user.email });
-    if (userData.length === 0)
-        return console.log('Cannot find user account. Create an account first.');
-    console.log('Success!! :' + userData);
+    const userData = await User.find({ password: user.password });
+    if (userData.length === 0) return false;
+    return userData;
 };
 
 /**
