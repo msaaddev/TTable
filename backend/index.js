8ec9ff786723be8e0840a5022a59295daf50c45b
msaaddev <mrsaadirfan@gmail.com>
2020-07-24 03:08:06 +0500

    📦 NEW: middleware

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const room = require('./routes/room');
 const courses = require('./routes/courses');
 const user = require('./routes/user');
 const schedule = require('./routes/schedule');
+const signup = require('./routes/signup');
 
 // middlewares
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use('/room', room);
 app.use('/courseinfo', courses);
 app.use('/login', user);
 app.use('/schedule', schedule);
+app.use('/', signup);
 
 // port
 const port = process.env.PORT || 5000;
