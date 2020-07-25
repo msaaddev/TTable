0e08e3ce486aa55cad18589a0fb1e84c9cf0eac2
msaaddev <mrsaadirfan@gmail.com>
2020-07-25 05:42:05 +0500

    🐛 FIX: await

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -3,15 +3,18 @@ const roomModel = require('../models/room');
 const router = express.Router();
 
 // route for sending rooms information in the database
-router.post('/', (req, res) => {
-    roomModel.createRoomData(req.body);
+router.post('/', async (req, res) => {
+    await roomModel.createRoomData(req.body);
+    res.send('Success');
 });
 
 // route for getting room information in the database
 router.get('/', async (req, res) => {
     const data = await roomModel.getRoomData(req.query.email);
     if (data !== false) res.send(data);
-    else res.send('false');
+    else {
+        res.send('false');
+    }
 });
 
 module.exports = router;
