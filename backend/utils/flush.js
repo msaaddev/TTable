43d8e5cdc1f3f5f6dc3f2eb9ac58722bf8dcc066
msaaddev <mrsaadirfan@gmail.com>
2020-07-25 05:41:29 +0500

    📦 NEW: flush file

---
+++ backend/utils/flush.js
@@ -0,0 +1,48 @@
+module.exports = {
+    reservedHrs: [
+        {
+            monday: [0, 0, 0, 0, 0],
+            tuesday: [0, 0, 0, 0, 0],
+            wednesday: [0, 0, 0, 0, 0],
+            thursday: [0, 0, 0, 0, 0],
+            friday: [0, 0, 0, 0, 0],
+        },
+        {
+            monday: [0, 0, 0, 0, 0],
+            tuesday: [0, 0, 0, 0, 0],
+            wednesday: [0, 0, 0, 0, 0],
+            thursday: [0, 0, 0, 0, 0],
+            friday: [0, 0, 0, 0, 0],
+        },
+        {
+            monday: [0, 0, 0, 0, 0],
+            tuesday: [0, 0, 0, 0, 0],
+            wednesday: [0, 0, 0, 0, 0],
+            thursday: [0, 0, 0, 0, 0],
+            friday: [0, 0, 0, 0, 0],
+        },
+    ],
+    availableHrs: [
+        {
+            monday: [1, 2, 3, 4, 5],
+            tuesday: [1, 2, 3, 4, 5],
+            wednesday: [1, 2, 3, 4, 5],
+            thursday: [1, 2, 3, 4, 5],
+            friday: [1, 2, 3, 4, 5],
+        },
+        {
+            monday: [1, 2, 3, 4, 5],
+            tuesday: [1, 2, 3, 4, 5],
+            wednesday: [1, 2, 3, 4, 5],
+            thursday: [1, 2, 3, 4, 5],
+            friday: [1, 2, 3, 4, 5],
+        },
+        {
+            monday: [1, 2, 3, 4, 5],
+            tuesday: [1, 2, 3, 4, 5],
+            wednesday: [1, 2, 3, 4, 5],
+            thursday: [1, 2, 3, 4, 5],
+            friday: [1, 2, 3, 4, 5],
+        },
+    ],
+};
