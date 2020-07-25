5c16790cd3d75ed17e4c3a2a7c1830ea36f23de0
msaaddev <mrsaadirfan@gmail.com>
2020-07-25 05:41:55 +0500

    👌 IMPROVE: scheduling

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,18 +1,99 @@
 const express = require('express');
-const courseModel = require('../models/courses');
 const router = express.Router();
+const courseModel = require('../models/courses');
+const roomModel = require('../models/room');
+const scheduleModel = require('../models/schedule');
+const algo = require('../utils/algo');
+const flush = require('../utils/flush');
 
-// route for sending courses into database
-router.post('/', (req, res) => {
-    courseModel.createCourseData(req.body);
+/* reserved timeslots for different sections */
+let reservedHrs = [
+    {
+        monday: [0, 0, 0, 0, 0],
+        tuesday: [0, 0, 0, 0, 0],
+        wednesday: [0, 0, 0, 0, 0],
+        thursday: [0, 0, 0, 0, 0],
+        friday: [0, 0, 0, 0, 0],
+    },
+    {
+        monday: [0, 0, 0, 0, 0],
+        tuesday: [0, 0, 0, 0, 0],
+        wednesday: [0, 0, 0, 0, 0],
+        thursday: [0, 0, 0, 0, 0],
+        friday: [0, 0, 0, 0, 0],
+    },
+    {
+        monday: [0, 0, 0, 0, 0],
+        tuesday: [0, 0, 0, 0, 0],
+        wednesday: [0, 0, 0, 0, 0],
+        thursday: [0, 0, 0, 0, 0],
+        friday: [0, 0, 0, 0, 0],
+    },
+];
+
+/* available timeslots for different section */
+
+let availableHrs = [
+    {
+        monday: [1, 2, 3, 4, 5],
+        tuesday: [1, 2, 3, 4, 5],
+        wednesday: [1, 2, 3, 4, 5],
+        thursday: [1, 2, 3, 4, 5],
+        friday: [1, 2, 3, 4, 5],
+    },
+    {
+        monday: [1, 2, 3, 4, 5],
+        tuesday: [1, 2, 3, 4, 5],
+        wednesday: [1, 2, 3, 4, 5],
+        thursday: [1, 2, 3, 4, 5],
+        friday: [1, 2, 3, 4, 5],
+    },
+    {
+        monday: [1, 2, 3, 4, 5],
+        tuesday: [1, 2, 3, 4, 5],
+        wednesday: [1, 2, 3, 4, 5],
+        thursday: [1, 2, 3, 4, 5],
+        friday: [1, 2, 3, 4, 5],
+    },
+];
+
+// route for sending courses and schedule into database
+router.post('/', async (req, res) => {
+    await courseModel.createCourseData(req.body);
+
+    const roomData = await roomModel.getRoomData(req.body.userAccount);
+    for (let i = 0; i < req.body.courseIDArr.length; i++) {
+        algo(
+            req.body.courseIDArr[i],
+            req.body.creditHrsArr[i],
+            roomData[0].sectionArr,
+            reservedHrs,
+            availableHrs
+        );
+    }
+    const obj = {
+        userAccount: req.body.userAccount,
+        schedule: reservedHrs,
+    };
+
+    availableHrs = JSON.parse(JSON.stringify(flush.availableHrs));
+    reservedHrs = JSON.parse(JSON.stringify(flush.reservedHrs));
+    await scheduleModel.createScheduleData(obj);
+    res.send('Success');
 });
 
 // route for getting course information in the database
 router.get('/', async (req, res) => {
+    console.log('course information');
     const data = await courseModel.getCourseData(req.query.email);
-    console.log(data);
-    if (data !== false) res.send(data);
-    else res.send('false');
+
+    console.log(data, 'course data');
+
+    if (data !== false) {
+        res.send(data);
+    } else {
+        res.send('false');
+    }
 });
 
 module.exports = router;
--- backend/routes/schedule.js
+++ backend/routes/schedule.js
@@ -1,99 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const roomModel = require('../models/room');
 const courseModel = require('../models/courses');
 const scheduleModel = require('../models/schedule');
-const algo = require('../utils/algo');
 const format = require('../utils/format');
-
-/* reserved timeslots for different sections */
-const reservedHrs = [
-    {
-        monday: [0, 0, 0, 0, 0],
-        tuesday: [0, 0, 0, 0, 0],
-        wednesday: [0, 0, 0, 0, 0],
-        thursday: [0, 0, 0, 0, 0],
-        friday: [0, 0, 0, 0, 0],
-    },
-    {
-        monday: [0, 0, 0, 0, 0],
-        tuesday: [0, 0, 0, 0, 0],
-        wednesday: [0, 0, 0, 0, 0],
-        thursday: [0, 0, 0, 0, 0],
-        friday: [0, 0, 0, 0, 0],
-    },
-    {
-        monday: [0, 0, 0, 0, 0],
-        tuesday: [0, 0, 0, 0, 0],
-        wednesday: [0, 0, 0, 0, 0],
-        thursday: [0, 0, 0, 0, 0],
-        friday: [0, 0, 0, 0, 0],
-    },
-];
-
-/* available timeslots for different section */
-
-const availableHrs = [
-    {
-        monday: [1, 2, 3, 4, 5],
-        tuesday: [1, 2, 3, 4, 5],
-        wednesday: [1, 2, 3, 4, 5],
-        thursday: [1, 2, 3, 4, 5],
-        friday: [1, 2, 3, 4, 5],
-    },
-    {
-        monday: [1, 2, 3, 4, 5],
-        tuesday: [1, 2, 3, 4, 5],
-        wednesday: [1, 2, 3, 4, 5],
-        thursday: [1, 2, 3, 4, 5],
-        friday: [1, 2, 3, 4, 5],
-    },
-    {
-        monday: [1, 2, 3, 4, 5],
-        tuesday: [1, 2, 3, 4, 5],
-        wednesday: [1, 2, 3, 4, 5],
-        thursday: [1, 2, 3, 4, 5],
-        friday: [1, 2, 3, 4, 5],
-    },
-];
 
 // route for getting schedule
 router.get('/', async (req, res) => {
-    const roomData = await roomModel.getRoomData(req.query.email);
+    console.log(req.query.email);
     const courseData = await courseModel.getCourseData(req.query.email);
+    const scheduleData = await scheduleModel.getScheduleData(req.query.email);
 
-    // if a schedule exist then sending it to the client side
-    if (req.query.flag === true) {
-        const data = await scheduleModel.getScheduleData(req.query.email);
-        const formatData = format(
-            data[0].schedule,
-            courseData[0].courseIDArr,
-            courseData[0].courseNameArr
-        );
-        res.send(formatData);
-    } else {
-        for (let i = 0; i < courseData[0].courseIDArr.length; i++)
-            algo(
-                courseData[0].courseIDArr[i],
-                courseData[0].creditHrsArr[i],
-                roomData[0].sectionArr,
-                reservedHrs,
-                availableHrs
-            );
+    const formatData = format(
+        scheduleData[0].schedule,
+        courseData[0].courseIDArr,
+        courseData[0].courseNameArr
+    );
 
-        const formatData = format(
-            reservedHrs,
-            courseData[0].courseIDArr,
-            courseData[0].courseNameArr
-        );
-
-        res.send(formatData);
-        const obj = {
-            userAccount: req.query.email,
-            schedule: reservedHrs,
-        };
-        scheduleModel.createScheduleData(obj);
-    }
+    res.send(formatData);
 });
 
 module.exports = router;
